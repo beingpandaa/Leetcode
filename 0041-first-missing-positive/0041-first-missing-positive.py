b54aea7ad3055a46858b93b8b34446610932d013
Tushar Sharma <65045642+beingpandaa@users.noreply.github.com>
2023-03-13 19:44:13 +0530

    Time: 318 ms (34.84%), Space: 27.3 MB (48.39%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.py
+++ 0041-first-missing-positive/0041-first-missing-positive.py
@@ -8,7 +8,8 @@ class Solution:
             index=abs(nums[i])-1
             if index>=0 and index<len(nums):
                 if nums[index]==0:
-                    nums[index]=-(index+1)
+                    nums[index]=-(index+1)  
+            #what we are trying to do here is that, we are directed to an index where value is 0,now we cant change the                                                  value 0 to negative as - 0 is 0 nly also we cant cchange it to a random number as it can also lead to direction                                              to a random index so we assign it a value which will direct it to the same index again or we can change it to a                                              value which is out of bound of our search space.
                 else:
                     nums[index]=-abs(nums[index])
                 
