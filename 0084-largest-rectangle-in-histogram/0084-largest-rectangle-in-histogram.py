3890651f49f6632723e53db1ac38104cdbeae6f9
Tushar Sharma <65045642+beingpandaa@users.noreply.github.com>
2023-03-16 13:57:44 +0530

    Time: 1189 ms (19.97%), Space: 27.9 MB (86.70%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -19,26 +19,56 @@ class Solution:
 #                 i+=1
 #             i+=1
 #         return ans
+
+
+#Approach2 Neetcode
+#         stack=[]
+#         index=0
+#         maxArea=0
+#         ele=(heights[index],index)
+#         while index<len(heights):
+#             # print(stack,maxArea)
+#             start=index
+#             while stack and stack[-1][0]>heights[index]:
+#                 h,i=stack.pop()
+#                 maxArea=max(maxArea,(index-i)*h)
+#                 start=i
+#             stack.append((heights[index],start))
+#             index+=1
+            
+#         while stack:
+#             u=stack.pop()
+#             maxArea=max(maxArea,(len(heights)-u[1])*u[0])
+#         return maxArea
+
+
+#Approach3- fron nearest smaller(optimizing approach 1)
         stack=[]
-        index=0
-        maxArea=0
-        ele=(heights[index],index)
-        while index<len(heights):
-            # print(stack,maxArea)
-            start=index
-            while stack and stack[-1][0]>heights[index]:
-                h,i=stack.pop()
-                maxArea=max(maxArea,(index-i)*h)
-                start=i
-            stack.append((heights[index],start))
-            index+=1
-            
-        while stack:
-            u=stack.pop()
-            maxArea=max(maxArea,(len(heights)-u[1])*u[0])
-        return maxArea
-                
-                
-            
-            
-        
+        ps=[]
+        ns=[0]*len(heights)
+        for i in range(len(heights)):
+            while stack and heights[stack[-1]]>=heights[i]:
+                        stack.pop()
+            if stack:
+                ps.append(stack[-1])
+            else:
+                ps.append(-1)
+            stack.append(i)
+        stack.clear()
+        for i in range(len(heights)-1,-1,-1):
+            while stack and heights[stack[-1]]>=heights[i]:
+                stack.pop()
+            if stack:
+                ns[i]=stack[-1]
+            else:
+                ns[i]=len(heights)
+
+            stack.append(i)
+        ar=0
+        for i in range(len(heights)):
+            ar=max(ar,(ns[i]-ps[i]-1)*heights[i])
+        return ar
+
+
+
+
