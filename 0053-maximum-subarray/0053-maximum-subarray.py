9e64edf6dd48363091c87ca9f65708b6909952eb
Tushar Sharma <65045642+beingpandaa@users.noreply.github.com>
2023-03-19 16:12:51 +0530

    Time: 775 ms (49.06%), Space: 28.7 MB (47.59%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,14 +4,13 @@ class Solution:
         j=0
         total=0
         maxSum=-10000
-        while j<len(nums):
-            if total<0:
-                total=0
-                i=j
-                
+        while j<len(nums):  
             total+=nums[j]
             maxSum=max(total,maxSum)
             j+=1
+            if total<0:
+                total=0
+                i=j
         return maxSum
             
                 
