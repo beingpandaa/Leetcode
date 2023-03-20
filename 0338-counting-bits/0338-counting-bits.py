deb4e98bbcc068ad1e0afb22722322165afee45e
Tushar Sharma <65045642+beingpandaa@users.noreply.github.com>
2023-03-21 00:24:47 +0530

    Time: 84 ms (75.16%), Space: 20.7 MB (66.99%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.py
+++ 0338-counting-bits/0338-counting-bits.py
@@ -1,13 +1,5 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        # def dfs(n,dp):
-        #     if n==1:return 1
-        #     if dp[n]==-1:
-        #         if n&1==0:
-        #             dp[n]=dfs(n>>1,dp)
-        #         else:
-        #             dp[n]=1+dfs(n-1,dp)
-        #     return dp[n]
         dp=[-1 for i in range(n+1)]
         for i in range(n+1):
             if i==0 or i==1:
