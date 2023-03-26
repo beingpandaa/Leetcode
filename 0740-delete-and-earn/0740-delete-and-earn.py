9a1c5ce99d0dc5d792c814d169893f51ca733b83
Tushar Sharma <65045642+beingpandaa@users.noreply.github.com>
2023-03-26 19:36:20 +0530

    Time: 90 ms (57.01%), Space: 20.5 MB (22.99%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -0,0 +1,22 @@
+class Solution:
+    def helper(self,nums,i,d,deepee):
+        if i==len(nums)-1:return nums[i]*d[nums[i]]
+        if i==len(nums):return 0
+        
+        ans1=nums[i]*d[nums[i]]
+        if nums[i+1]==nums[i]+1:
+            if deepee[i+2]==-1:deepee[i+2]=self.helper(nums,i+2,d,deepee)
+            ans1+=deepee[i+2]
+        else:
+            if deepee[i+1]==-1:deepee[i+1]=self.helper(nums,i+1,d,deepee)
+            ans1+=deepee[i+1]
+        if deepee[i+1]==-1:deepee[i+1]=self.helper(nums,i+1,d,deepee)
+        ans2=deepee[i+1]
+        return max(ans1,ans2)
+        
+        
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        d=Counter(nums)
+        nums=sorted(list(set(nums)))
+        deepee=[-1 for i in range(len(nums)+1)]
+        return self.helper(nums,0,d,deepee)
