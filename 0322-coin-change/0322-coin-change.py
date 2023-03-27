b8a6505457f105b6e8584f40ebd6e1e57e4b14d5
Tushar Sharma <65045642+beingpandaa@users.noreply.github.com>
2023-03-27 20:48:14 +0530

    Time: 2605 ms (5.79%), Space: 24.8 MB (24.50%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -19,27 +19,27 @@ class Solution:
                     
                 
                 
-#     def helper(self,coins,i,total,dp):
-#         if total==0:return 0
-#         if total<0 or i==len(coins):return sys.maxsize
+    def helper(self,coins,i,total,dp):
+        if total==0:return 0
+        if total<0 or i==len(coins):return sys.maxsize
         
-#         if total-coins[i]<0:ans1=sys.maxsize
-#         else:
+        if total-coins[i]<0:ans1=sys.maxsize
+        else:
         
-#             if dp[i][total-coins[i]]==-1 :
-#                 dp[i][total-coins[i]]=self.helper(coins,i,total-coins[i],dp)
-#             ans1=1+dp[i][total-coins[i]]
-#         if dp[i+1][total]==-1 :
-#                 dp[i+1][total]=self.helper(coins,i+1,total,dp)
+            if dp[i][total-coins[i]]==-1 :
+                dp[i][total-coins[i]]=self.helper(coins,i,total-coins[i],dp)
+            ans1=1+dp[i][total-coins[i]]
+        if dp[i+1][total]==-1 :
+                dp[i+1][total]=self.helper(coins,i+1,total,dp)
             
-#         ans2=dp[i+1][total]
+        ans2=dp[i+1][total]
         
-#         return min(ans1,ans2)
+        return min(ans1,ans2)
         
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp=[[-1 for i in range(amount+1)] for i in range(len(coins)+1)]
-        ans=self.helperitr(coins,dp)
-        # ans=self.helper(coins,0,amount,dp)
+        # ans=self.helperitr(coins,dp)
+        ans=self.helper(coins,0,amount,dp)
         if ans>=sys.maxsize:return -1
         return ans
     
