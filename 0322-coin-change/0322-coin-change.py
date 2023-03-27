37827489ef3d617fdcae838fc78de8138c726f88
Tushar Sharma <65045642+beingpandaa@users.noreply.github.com>
2023-03-27 20:47:17 +0530

    Time: 3292 ms (5.00%), Space: 15.4 MB (29.87%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,4 +1,24 @@
 class Solution:
+    def helperitr(self,coins,dp):
+        for i in range(len(dp)):
+            dp[i][0]=0
+        for j in range(len(dp[0])):
+            dp[len(dp)-1][j]=sys.maxsize
+        minVal=sys.maxsize
+        for i in range(len(dp)-1,-1,-1):
+            for amt in range(len(dp[0])):
+                if dp[i][amt]==-1:
+                    if amt-coins[i]<0:
+                        ans1=sys.maxsize
+                    else:
+                        ans1=1+dp[i][amt-coins[i]]
+                    ans2=dp[i+1][amt]
+                    dp[i][amt]=min(ans1,ans2)
+                if amt==len(dp[0])-1:minVal=min(minVal,dp[i][amt])
+        return minVal
+                    
+                
+                
     def helper(self,coins,i,total,dp):
         if total==0:return 0
         if total<0 or i==len(coins):return sys.maxsize
@@ -18,6 +38,8 @@ class Solution:
         
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp=[[-1 for i in range(amount+1)] for i in range(len(coins)+1)]
-        ans=self.helper(coins,0,amount,dp)
+        ans=self.helperitr(coins,dp)
+        # ans=self.helper(coins,0,amount,dp)
         if ans>=sys.maxsize:return -1
         return ans
+    
