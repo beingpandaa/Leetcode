15827811acb1237003054a03012c46b413053630
Tushar Sharma <65045642+beingpandaa@users.noreply.github.com>
2023-03-29 14:45:40 +0530

    Time: 44 ms (64.24%), Space: 13.8 MB (98.73%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.py
+++ 1402-reducing-dishes/1402-reducing-dishes.py
@@ -3,16 +3,14 @@ class Solution:
         n=len(arr)
         arr.sort()
         total=arr[n-1]*n
-        
+        if total<0 :return 0
         for i in range(n-2,-1,-1):
-            
             total+=arr[i]*(i+1)
             arr[i]+=arr[i+1]
         i=0
         while i<n and total<total-arr[i] :
             total=total-arr[i]
             i+=1
-        if i==n:return 0
         return total
             
             
