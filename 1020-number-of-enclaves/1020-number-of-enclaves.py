f5d9e2ed61d5784be9bac03ea4a4dad10a511514
Tushar Sharma <65045642+beingpandaa@users.noreply.github.com>
2023-04-08 01:22:57 +0530

    Time: 686 ms (64.72%), Space: 75.7 MB (62.38%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -12,7 +12,7 @@ class Solution:
         #     print(ele)
         for i in range (len(grid)):
             for j in range (len(grid[0])):
-                if i==0 or i==len(grid)-1 or j==0 or j==len(grid[0])-1:
+                if (i==0 or i==len(grid)-1 or j==0 or j==len(grid[0])-1) and grid[i][j]==1:
                     self.helper(grid,i,j)
         count=0
         # for ele in grid:
