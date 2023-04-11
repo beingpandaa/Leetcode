48c45fcccaa6ec5d75e023f7c4f7d9387b46a3e1
Tushar Sharma <65045642+beingpandaa@users.noreply.github.com>
2023-04-11 18:51:18 +0530

    Time: 811 ms (65.37%), Space: 26 MB (73.89%) - LeetHub

---
+++ 2439-minimize-maximum-of-array/2439-minimize-maximum-of-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimizeArrayValue(self, nums: List[int]) -> int:
+        ans=nums[0]
+        total=nums[0]
+        for i in range(1,len(nums)):
+            total+=nums[i]
+            avg=math.ceil(total/(i+1))
+            ans=max(ans,avg)
+        return ans
+            
