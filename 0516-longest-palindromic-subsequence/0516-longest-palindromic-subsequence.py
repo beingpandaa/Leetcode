2e9202f9dd3be0ca5b26612e18909b7dc21d66b3
Tushar Sharma <65045642+beingpandaa@users.noreply.github.com>
2023-04-14 13:28:23 +0530

    Time: 1288 ms (77.82%), Space: 93.5 MB (23.06%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.py
@@ -1,17 +1,17 @@
 class Solution:
-    def itr(self,s):
-        n = len(s)
-        dp = [[0] * n for _ in range(n)]
+#     def itr(self,s):
+#         n = len(s)
+#         dp = [[0] * n for _ in range(n)]
 
-        for i in range(n - 1, -1, -1):
-            dp[i][i] = 1
-            for j in range(i + 1, n):
-                if s[i] == s[j]:
-                    dp[i][j] = dp[i + 1][j - 1] + 2
-                else:
-                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+#         for i in range(n - 1, -1, -1):
+#             dp[i][i] = 1
+#             for j in range(i + 1, n):
+#                 if s[i] == s[j]:
+#                     dp[i][j] = dp[i + 1][j - 1] + 2
+#                 else:
+#                     dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
 
-        return dp[0][n - 1]
+#         return dp[0][n - 1]
     def helper(self,s,i,j,d):
         if i>j:return 0
         if i==j:return 1
@@ -25,5 +25,5 @@ class Solution:
             d[(i,j)]= max(ans1,ans2)
             return d[(i,j)]
     def longestPalindromeSubseq(self, s: str) -> int:
-        return self.itr(s)
-        # return self.helper(s,0,len(s)-1,d={})
+        # return self.itr(s)
+        return self.helper(s,0,len(s)-1,d={})
