1fc4ee8ed0c3f27448cc34a314b2f4c8ebeb90b3
Tushar Sharma <65045642+beingpandaa@users.noreply.github.com>
2023-04-17 16:17:46 +0530

    Time: 3409 ms (25.88%), Space: 82.6 MB (47.84%) - LeetHub

--- 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.py
+++ 1639-number-of-ways-to-form-a-target-string-given-a-dictionary/1639-number-of-ways-to-form-a-target-string-given-a-dictionary.py
@@ -1,21 +1,18 @@
 class Solution:
-    def numWays(self, words: List[str], target: str) -> int:
-        alphabet = 26
-        mod = 1000000007
-        m = len(target)
-        k = len(words[0])
-        cnt = [[0] * k for _ in range(alphabet)]
-        for j in range(k):
-            for word in words:
-                cnt[ord(word[j]) - ord('a')][j] += 1
-        dp = [[0] * (k + 1) for _ in range(m + 1)]
-        dp[0][0] = 1
-        for i in range(m + 1):
-            for j in range(k):
-                if i < m:
-                    dp[i + 1][j + 1] += (cnt[ord(target[i]) - ord('a')][j]
-                                         * dp[i][j])
-                    dp[i + 1][j + 1] %= mod
-                dp[i][j + 1] += dp[i][j]
-                dp[i][j + 1] %= mod
-        return dp[m][k]
+    def helper(self,d,tar,words,i,k,dp):
+        if i==len(tar):return 1
+        if k==len(words[0]):return 0
+        if dp[i][k+1]==-1:dp[i][k+1]=self.helper(d,tar,words,i,k+1,dp)
+        ans1=dp[i][k+1]
+        ans2=0
+        if (k,tar[i]) in d:
+            if dp[i+1][k+1]==-1:dp[i+1][k+1]=self.helper(d,tar,words,i+1,k+1,dp)
+            ans2=d[(k,tar[i])]*dp[i+1][k+1]
+        return ans1+ans2
+    def numWays(self, words: List[str], tar: str) -> int:
+        dp=[[-1 for k in range(len(words[0])+1)] for i in range(len(tar)+1)]
+        d={}
+        for i in range(len(words)):
+            for j in range(len(words[0])):
+                d[(j,words[i][j])]=d.get((j,words[i][j]),0)+1
+        return self.helper(d,tar,words,0,0,dp)%1000000007
