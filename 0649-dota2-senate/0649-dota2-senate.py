8fbcdf99ac4f651a92124bbaea504f4da41b8ab3
Tushar Sharma <65045642+beingpandaa@users.noreply.github.com>
2023-05-04 15:57:50 +0530

    Time: 91 ms (32.11%), Space: 16.5 MB (11.62%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -0,0 +1,31 @@
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        de=collections.deque(list(senate))
+        r_count = senate.count('R')
+        d_count = len(senate) - r_count
+        r=0
+        d=0
+        while de:
+            
+            print(de)
+            for i in range(len(de)):
+                ele=de.popleft()
+                if ele=='R':
+                    if d>0:
+                        d-=1
+                        r_count-=1
+                    else:
+                        r+=1
+                        de.append(ele)
+                else:
+                    if r>0:
+                        r-=1
+                        d_count-=1
+                    else:
+                        d+=1
+                        de.append(ele)
+            
+            if d_count==0:return "Radiant"
+            elif r_count==0: return "Dire"
+                    
+            
