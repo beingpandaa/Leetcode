2d129a0bc3ec28e9a77be726b1e2caf83176c437
Tushar Sharma <65045642+beingpandaa@users.noreply.github.com>
2023-06-08 14:19:11 +0530

    Time: 131 ms (53.17%), Space: 17.6 MB (14.16%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.py
@@ -1,6 +1,5 @@
 class Solution:
     def binSearch(self,low,high,row,grid):
-        # if we are to search in column, we gonna set flag True else, we gonna set it False.
         if grid[row][low]<0:return -1
         while low<high:
             mid=(low+high+1)//2
@@ -9,9 +8,7 @@ class Solution:
             elif grid[row][mid]<=-1:
                 high=mid-1
         return low
-            
-            
-            
+    
     def countNegatives(self, grid: List[List[int]]) -> int:
         n,m=len(grid),len(grid[0])
         count=0
