81d757e89fbf6fbd28070299b7788ec707d278a1
Tushar Sharma <65045642+beingpandaa@users.noreply.github.com>
2023-06-14 13:48:49 +0530

    Time: 120 ms (5.10%), Space: 18.6 MB (42.66%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
@@ -1,17 +1,29 @@
 class Solution:
+    def morris_traversal(self,root):
+        current = root
+        op=[]
+        while current:
+            if current.left is None:
+                op.append(current.val)
+                current = current.right
+            else:
+                # Find the inorder predecessor of current
+                pre = current.left
+                while pre.right and pre.right != current:
+                    pre = pre.right
+
+                # Make current as the right child of its inorder predecessor
+                if pre.right is None:
+                    pre.right = current
+                    current = current.left
+                # Revert the changes made to restore the original tree and print current node
+                else:
+                    pre.right = None
+                    op.append(current.val)
+                    current = current.right
+        return op
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
-        op = []
-
-        def dfs(r):
-            if r is None:
-                return
-            op.append(r.val)
-            dfs(r.left)
-            dfs(r.right)
-        
-        dfs(root)
-
-        op.sort()
+        op = self.morris_traversal(root)
         diff = 9999999999
         for i in range(1, len(op)):
             diff = min(diff, op[i] - op[i-1])
