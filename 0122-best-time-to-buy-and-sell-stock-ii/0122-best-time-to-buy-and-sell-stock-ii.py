e05f157def3b1dead1d027246411c472e99a3cd7
Tushar Sharma <65045642+beingpandaa@users.noreply.github.com>
2023-06-22 16:32:07 +0530

    Time: 124 ms (5.25%), Space: 47.5 MB (5.10%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,11 +1,29 @@
 class Solution:
+    def helper(self,prices,i,buy,dp):
+        if i==len(prices):
+            return 0
+        
+        if not buy:
+            if dp[i+1][1]==-1:dp[i+1][1]=self.helper(prices,i+1,1,dp)
+            prof1=-prices[i]+dp[i+1][1]
+            if dp[i+1][0]==-1:dp[i+1][0]=self.helper(prices,i+1,0,dp)
+            prof2=dp[i+1][0]
+        else:
+            if dp[i+1][0]==-1:dp[i+1][0]=self.helper(prices,i+1,0,dp)
+            prof1=prices[i]+dp[i+1][0]
+            if dp[i+1][1]==-1:dp[i+1][1]=self.helper(prices,i+1,1,dp)
+            prof2=dp[i+1][1]
+        return max(prof1,prof2)
+            
     def maxProfit(self, prices: List[int]) -> int:
-        profit=0
-        curr_prof=0
-        buy_at=prices[0]
-        for i in range(len(prices)):
-            if prices[i]>buy_at:
-                profit+=prices[i]-buy_at 
-            buy_at=prices[i]         
-        return profit
+        # profit=0
+        # curr_prof=0
+        # buy_at=prices[0]
+        # for i in range(len(prices)):
+        #     if prices[i]>buy_at:
+        #         profit+=prices[i]-buy_at 
+        #     buy_at=prices[i]         
+        # return profit
+        dp=[[-1 for i in range (2)] for j in range(len(prices)+1)]
+        return self.helper(prices,0,0,dp)
                 
