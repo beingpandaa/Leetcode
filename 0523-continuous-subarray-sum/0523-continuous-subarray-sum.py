f375aa9dc47d0e583735168fe4656a20fcd2667c
Tushar Sharma <65045642+beingpandaa@users.noreply.github.com>
2026-09-17 19:10:53 +0530

    Time: 88 ms (8.14%), Space: 38.5 MB (96.93%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def checkSubarraySum(self, nums: list[int], k: int) -> bool:
+        d = {nums[0]%k:1}
+        for i in range(1,len(nums)):
+            rem = nums[i] % k
+            nums[i] = nums[i]+nums[i-1]
+            prefixRem = nums[i]%k
+            if prefixRem == 0 : return True
+            if rem == 0 : 
+                if d.get(prefixRem,0)>1: return True
+            else:
+                if prefixRem == 0 or prefixRem in d: return True
+            d[prefixRem] = d.get(prefixRem,0)+1
+        return False
+
