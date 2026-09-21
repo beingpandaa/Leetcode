5d6594d26dcea4279f0e714cd7e36edc127bcad2
Tushar Sharma <65045642+beingpandaa@users.noreply.github.com>
2026-09-21 15:57:54 +0530

    Time: 7 ms (60.9%), Space: 21.3 MB (13.88%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,14 +1,13 @@
 class Solution:
-    def majorityElement(self, nums: List[int]) -> int:
-        d={}
-        maxf=0
-        max_ele=nums[0]
-        for ele in nums:
-            d[ele]=d.get(ele,0)+1
-            if d[ele]>maxf:
-                maxf=d[ele]
-                max_ele=ele
-        return max_ele
-                
-            
-            
+    def majorityElement(self, nums: list[int]) -> int:
+        ele = nums[0]
+        count = 0
+        for i in range(len(nums)):
+            if count ==0:
+                ele = nums[i]
+                count+=1
+            elif nums[i]==ele:
+                count+=1
+            else:
+                count-=1
+        return ele
